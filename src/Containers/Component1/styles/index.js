ce38497a33efc1f7b4171dd60ebc7cb1b91f96d7
Vaishali2127 <vaishalimahour8@gmail.com>
2021-10-27 19:11:17 +0530

    seventh commit

--- src/Containers/Component1/styles/index.js
+++ src/Containers/Component1/styles/index.js
@@ -11,11 +11,49 @@ IntroSection.mainWrapper = styled.div`
   .box {
     width: 150px;
     height: 150px;
-    background: red;
+    background: skyblue;
     margin-top: 20px;
+    animation: anim 10s infinite;
+    animation-direction: alternate;
     :hover {
       transition: background-color 1s ease-in-out;
       background-color: green;
+    }
+    @keyframes anim {
+      0% {
+        width: 150px;
+        height: 150px;
+      }
+
+      25% {
+        border-radius: 0%;
+        width: 0px;
+        height: 0px;
+      }
+
+      50% {
+        width: 200px;
+        height: 200px;
+      }
+      75% {
+        width: 0px;
+        height: 0px;
+      }
+
+      100% {
+        border-radius: 100%;
+        opacity: 0.5;
+        width: 150px;
+        height: 150px;
+        background: radial-gradient(
+          circle,
+          red,
+          orange,
+          yellow,
+          skyblue,
+          purple
+        );
+      }
     }
   }
 `;

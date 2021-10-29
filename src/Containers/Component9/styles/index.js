5282bf3666a851cc1187020228912ba6fd14cf21
Vaishali2127 <vaishalimahour8@gmail.com>
2021-10-29 22:59:19 +0530

    Add section 9

--- src/Containers/Component9/styles/index.js
+++ src/Containers/Component9/styles/index.js
@@ -19,10 +19,56 @@ IntroSection.MainWrapper = styled.div`
   background: #307d8b;
   color: white;
   border-radius: 50%;
-  transition: background 1s, transform 1s 0.1s ease-in-out;
+  transition: background 1s, transform 1s ease-in-out;
 
   :hover {
-    background: #000;
-    transform: rotate(360deg);
+    background: pink;
+    // transform: rotate(360deg);
+    transform: scale(1.2);
+    animation: button 2s 1;
+    animation-direction: alternate;
+
+    @keyframes button {
+      0% {
+        transform: skew(180deg);
+      }
+      15% {
+        transform: rotate(60deg);
+      }
+      30% {
+        transform: rotate(-60deg);
+      }
+      45% {
+        transform: rotate(-360deg);
+      }
+
+      // MOVING  BALL
+      // 60% {
+      //   transform: translate(100px, 0);
+      // }
+      // 75% {
+      //   transform: translate(100px, 100px);
+      // }
+      // 90% {
+      //   transform: translate(-100px, 0);
+      // }
+      // 100% {
+      //   transform: translate(-100px, -100);
+      // }
+
+      // JUMPING BALL
+      60% {
+        transform: translate(0, -100px);
+      }
+      75% {
+        transform: translate(0, 0);
+      }
+      90% {
+        transform: translate(0, -110px);
+      }
+      100% {
+        transform: translate(0, 0);
+      }
+    }
   }
 `;
